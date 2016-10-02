3dad6913fff88c99fc77136117010f3f2e91bd12
MrTrollNugNug <cazawadzki@comcast.net>
2016-10-02 18:32:05 -0400

    Updated LightAir to 1.10.2
    Enjoy!

--- src/main/java/com/lewismcreu/lightair/LightAir.java
+++ src/main/java/com/lewismcreu/lightair/LightAir.java
@@ -2,12 +2,7 @@ package com.lewismcreu.lightair;
 
 import com.lewismcreu.lightair.proxy.CommonProxy;
 import com.lewismcreu.lightair.proxy.IProxy;
-
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 public class LightAir
@@ -17,13 +12,13 @@ public class LightAir
 	public static LightAir instance;
 
 	// Sided Proxy Declaration
-	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+	@net.minecraftforge.fml.common.SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static IProxy proxy;
 
 	// Pre Initialization events
 	// Configuration, Items, Blocks
 	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event)
+	public void preInit(net.minecraftforge.fml.common.event.FMLInitializationEvent event)
 	{
 		// Initialize CommonProxy
 		CommonProxy.init();
@@ -31,14 +26,14 @@ public class LightAir
 
 	// Initialization events
 	@Mod.EventHandler
-	public void init(FMLInitializationEvent event)
+	public void init(net.minecraftforge.fml.common.event.FMLInitializationEvent event)
 	{
 
 	}
 
 	// Post Initialization events
 	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event)
+	public void postInit(net.minecraftforge.fml.common.event.FMLInitializationEvent event)
 	{
 
 	}
--- src/main/java/com/lewismcreu/lightair/Reference.java
+++ src/main/java/com/lewismcreu/lightair/Reference.java
@@ -4,7 +4,8 @@ public class Reference
 {
 	public static final String MOD_ID = "LightAir";
 	public static final String MOD_NAME = "Light Air";
-	public static final String MOD_VERSION = "1.7.10-0.1";
+	public static final String MOD_VERSION = "1.10.2-0.1";
 	public static final String CLIENT_PROXY_CLASS = "com.lewismcreu.lightair.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.lewismcreu.lightair.proxy.ServerProxy";
 }
+

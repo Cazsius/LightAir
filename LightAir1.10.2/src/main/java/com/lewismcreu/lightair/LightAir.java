6e5b05ab321d1a78c9e3ab911669028ee5712a51
Lewis_McReu <wim.creuwels@hotmail.com>
2016-11-16 22:19:09 +0100

    Defaulted version number to fall back to mcmod.info

--- LightAir1.10.2/src/main/java/com/lewismcreu/lightair/LightAir.java
+++ LightAir1.10.2/src/main/java/com/lewismcreu/lightair/LightAir.java
@@ -14,17 +14,17 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = LightAir.MOD_ID, name = LightAir.MOD_NAME, version = LightAir.MOD_VERSION)
+@Mod(modid = LightAir.MOD_ID, name = LightAir.MOD_NAME)
 public class LightAir
 {
 	public static final String MOD_ID = "lightair";
 	public static final String MOD_NAME = "Light Air";
-	public static final String MOD_VERSION = "1.0";
 
 	@Mod.Instance("lightair")
 	public static LightAir instance;
 
-	@SidedProxy(clientSide = "com.lewismcreu.lightair.ClientProxy", serverSide = "com.lewismcreu.lightair.CommonProxy")
+	@SidedProxy(clientSide = "com.lewismcreu.lightair.ClientProxy",
+			serverSide = "com.lewismcreu.lightair.CommonProxy")
 	public static CommonProxy proxy;
 
 	public static SimpleNetworkWrapper channel;
@@ -34,7 +34,8 @@ public class LightAir
 	{
 		proxy.preInit();
 		channel = NetworkRegistry.INSTANCE.newSimpleChannel(MOD_ID);
-		channel.registerMessage(OpenGuiMessage.Handler.class, OpenGuiMessage.class, 0, Side.SERVER);
+		channel.registerMessage(OpenGuiMessage.Handler.class,
+				OpenGuiMessage.class, 0, Side.SERVER);
 		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
 	}
 
@@ -47,24 +48,28 @@ public class LightAir
 	public class GuiHandler implements IGuiHandler
 	{
 		@Override
-		public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+		public Object getServerGuiElement(int ID, EntityPlayer player,
+				World world, int x, int y, int z)
 		{
 			if (ID == 0)
 			{
 				ItemStack s = player.getHeldItemMainhand();
-				if (s != null && s.getItem() == Item.getItemFromBlock(CommonProxy.blockAirLight))
+				if (s != null && s.getItem() == Item
+						.getItemFromBlock(CommonProxy.blockAirLight))
 					return new ContainerLightAir(s);
 			}
 			return null;
 		}
 
 		@Override
-		public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+		public Object getClientGuiElement(int ID, EntityPlayer player,
+				World world, int x, int y, int z)
 		{
 			if (ID == 0)
 			{
 				ItemStack s = player.getHeldItemMainhand();
-				if (s != null && s.getItem() == Item.getItemFromBlock(CommonProxy.blockAirLight))
+				if (s != null && s.getItem() == Item
+						.getItemFromBlock(CommonProxy.blockAirLight))
 					return new GuiLightAir(new ContainerLightAir(s));
 			}
 			return null;
